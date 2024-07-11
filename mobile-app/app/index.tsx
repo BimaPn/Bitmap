666c8d3bb4fc2b2333b4c256bb7d8fcb5ba3f4dc
lonewolf <bimaptr12@gmail.com>
2024-07-11 12:50:04 +0700

    Initial REAL mobile project.

---
+++ mobile-app/app/index.tsx
@@ -0,0 +1,16 @@
+import { StatusBar } from "expo-status-bar";
+import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+const Welcome = () => {
+
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <Text>Fuck you bitch</Text> 
+
+    </SafeAreaView>
+  );
+};
+
+export default Welcome;
