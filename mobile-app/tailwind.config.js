8627222a480306f81aa68587588e01c3f1ae4d19
lonewolf <bimaptr12@gmail.com>
2024-07-11 11:13:04 +0700

    Replaced the mobile project folder to the new one because it's broken.

--- mobile-app/tailwind.config.js
+++ mobile-app/tailwind.config.js
@@ -2,23 +2,7 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#161622",
-        secondary: {
-          DEFAULT: "#FF9C01",
-          100: "#FF9001",
-          200: "#FF8E01",
-        },
-        black: {
-          DEFAULT: "#000",
-          100: "#1E1E2D",
-          200: "#232533",
-        },
-        gray: {
-          100: "#CDCDE0",
-        },
-      },
+    extend: { 
       fontFamily: {
         pthin: ["Poppins-Thin", "sans-serif"],
         pextralight: ["Poppins-ExtraLight", "sans-serif"],
@@ -33,4 +17,5 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
+
