51596b78f8b5295a5a13eef5672bfbf93ad60c59
lonewolf <bimaptr12@gmail.com>
2024-07-21 10:25:26 +0700

    Add user item component and completed user's following page content.

--- mobile-app/app/users/[username]/following.tsx
+++ mobile-app/app/users/[username]/following.tsx
@@ -1,24 +1,19 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, ScrollView } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import BackHeader from '../../../components/BackHeader'
-import { images } from '../../../constants'
+import UserItem from '../../../components/user/UserItem'
 
 const following = () => {
   return (
     <SafeAreaView> 
-      <ScrollView stickyHeaderIndices={[0]}> 
+      <ScrollView className='h-full bg-white' stickyHeaderIndices={[0]}> 
         <BackHeader title='25k Following' />
 
-        <View> 
-          <View className='flex-row justify-between'  >
-            <View className='flex-row items-center space-x-1'> 
-              <Image source={images.user} className='w-7 h-7' resizeMode='cover' />
-              <View> 
-              <Text>Udin Sarwendah</Text>
-              <Text>@udah22</Text>
-              </View>
-            </View>
-          </View>
+        <View className='px-3 mt-3'> 
+          {[1,2,3,4,5,6,7,8].map((item) => ( 
+            <UserItem containerStyles='mb-4' key={item} />
+          ))}
+
         </View>
 
       </ScrollView>
---
+++ mobile-app/components/user/UserItem.tsx
@@ -0,0 +1,22 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { images } from '../../constants'
+
+const UserItem = ({containerStyles}:{containerStyles?: string}) => {
+  return (
+    <View className={`flex-row justify-between items-center ${containerStyles}`}>
+      <View className='flex-row items-center space-x-3'> 
+        <Image source={images.user} className='w-14 h-14 rounded-full' resizeMode='cover' />
+        <View> 
+        <Text numberOfLines={1} className='font-pmedium text-base'>Udin Sarwendah</Text>
+        <Text className='text-netral'>@udah22</Text>
+        </View>
+      </View>
+
+      <TouchableOpacity className='w-20 h-11 items-center justify-center bg-semiLight rounded-xl'> 
+        <Text className='text-center font-pmedium'>Follow</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default UserItem
