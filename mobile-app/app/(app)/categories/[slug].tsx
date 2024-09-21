397a56c791769121640bf3cc6b18719733f79e24
lonewolf <bimaptr12@gmail.com>
2024-09-21 21:24:52 +0700

    Get category's posts from server.

--- mobile-app/app/(app)/categories/[slug].tsx
+++ mobile-app/app/(app)/categories/[slug].tsx
@@ -4,9 +4,33 @@ import { MasonryFlashList } from '@shopify/flash-list'
 import BackHeader from '../../../components/BackHeader'
 import Post from '../../../components/Post'
 import { useCategory } from '../../../components/providers/CategoryProvider'
+import { useEffect, useState } from 'react'
+import ApiClient from '../../../api/axios/ApiClient'
+import { useLocalSearchParams } from 'expo-router'
+import LoadingSpinner from '../../../components/LoadingSpinner'
+import NoResult from '../../../components/NoResult'
 
 const CategoryDetail = () => {
+  const { slug } = useLocalSearchParams();
+
   const { category } = useCategory()
+  const [posts, setposts] = useState<null | PostProps[]>(null)
+
+  useEffect(() => {
+    const getPosts = async () => {
+      ApiClient().get(`/api/categories/${slug}/posts`)
+      .then((res) => {
+        const result = res.data.posts
+        setposts(result)
+      })
+      .catch((err) => {
+        console.log(err.response)
+      })
+    }
+
+    getPosts()
+  },[])
+
   return category && (
     <SafeAreaView className='h-full bg-white'> 
       <ScrollView stickyHeaderIndices={[0]}> 
@@ -15,23 +39,32 @@ const CategoryDetail = () => {
           <View className='mx-3 mt-3 mb-4'> 
             <Text className='text-base'>{category.description}</Text>
           </View>
-        {/*  <MasonryFlashList
+          {!posts && (
+            <LoadingSpinner />
+          )}
+
+          {posts && posts.length < 1 && (
+            <NoResult />
+          )}
+
+          {posts && posts.length > 0 && (
+            <MasonryFlashList
             contentContainerStyle={{ 
               paddingHorizontal: 8,
               backgroundColor: "#FFFFFF",
             }}
-            data={imagesExample}
+            data={posts}
             keyExtractor={(item) => item.id}
             numColumns={2}
             renderItem={({ item }) => ( 
               <Post
-              id={item.id}
-              image={item.image} 
+              post={item}
               containerStyles='m-1' 
               /> 
             )}
             estimatedItemSize={200}
-          />*/}
+            />
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
