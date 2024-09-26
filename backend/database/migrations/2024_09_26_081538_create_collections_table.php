370c82d5dc9de81bffbc7d29b035b94b18fae6b3
lonewolf <bimaptr12@gmail.com>
2024-09-26 15:19:23 +0700

    add collection and post_collection model.

---
+++ backend/database/migrations/2024_09_26_081538_create_collections_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('collections', function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->foreignUuid("user_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('collections');
+    }
+};
---
+++ backend/database/seeders/PostCollectionSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostCollectionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
